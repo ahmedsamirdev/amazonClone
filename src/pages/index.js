298ec71aafea1d1d0f4db594befa1f3821d16469
unknown <ahmedsamir551@gmail.com>
2021-05-25 21:52:37 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,30 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
+import { useState, useEffect } from "react";
 
 export default function Home({ products }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
   return (
-    <div className="bg-gray-100">
-      <Head>
-        <title>Amazon Clone</title>
-      </Head>
-      <Header />
-      <main className="mx-auto max-w-screen-2xl">
-        {/* Banner */}
-        <Banner />
-        
-        {/* Products */}
-       <ProductFeed products={products} /> 
-      </main>
-    </div>
+    <>
+      {mounted && (
+        <div className="bg-gray-100">
+          <Head>
+            <title>Amazon Clone</title>
+          </Head>
+          <Header />
+          <main className="mx-auto max-w-screen-2xl">
+            {/* Banner */}
+            <Banner />
+
+            {/* Products */}
+            <ProductFeed products={products} />
+          </main>
+        </div>
+      )}
+    </>
   );
 }
 
