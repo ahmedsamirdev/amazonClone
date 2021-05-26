d3a0390502fb417d88de72a9aa5970cb21747037
unknown <ahmedsamir551@gmail.com>
2021-05-26 06:44:47 +0200

    firebase

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAfLaI8ziucf3NJ4K-KGLhNsAQx-bwGKC4",
+  authDomain: "clone-30f9d.firebaseapp.com",
+  projectId: "clone-30f9d",
+  storageBucket: "clone-30f9d.appspot.com",
+  messagingSenderId: "599269899051",
+  appId: "1:599269899051:web:5e744a90a52b48dc36e36e",
+};
