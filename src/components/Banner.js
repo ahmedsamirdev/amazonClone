91e9b662d990ef444db3262f511c51129633f522
unknown <ahmedsamir551@gmail.com>
2021-05-27 02:38:09 +0200

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,11 @@
 import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default function Banner() {
   return (
-    <div className="relative ">
-      <div className="absolute bottom-0 z-20 w-full h-52 bg-gradient-to-t from-gray-100 to-transparent" />
+    <div className="relative flex justify-center">
+      <div className="absolute  bottom-0 z-20 w-full h-52 bg-gradient-to-t from-gray-100 to-transparent" />
       <Carousel
         autoPlay
         infiniteLoop
@@ -11,18 +13,19 @@ export default function Banner() {
         showIndicators={false}
         showThumbs={false}
         interval={5000}
+        width={1500}
       >
         <div>
-          <img src="/1.jpg" alt="" loading="lazy" />
+          <Image width={1500} height={600} src="/1.jpg" alt="" loading="lazy" />
         </div>
         <div>
-          <img src="/2.jpg" alt="" loading="lazy" />
+          <Image width={1500} height={600} src="/2.jpg" alt="" loading="lazy" />
         </div>
         <div>
-          <img src="/3.jpg" alt="" loading="lazy" />
+          <Image width={1500} height={600} src="/3.jpg" alt="" loading="lazy" />
         </div>
         <div>
-          <img src="/4.jpg" alt="" loading="lazy" />
+          <Image width={1500} height={600} src="/4.jpg" alt="" loading="lazy" />
         </div>
       </Carousel>
     </div>
